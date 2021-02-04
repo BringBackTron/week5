0ddc418af8e82da0db23994d53478f46671c9f50
George McMullen <gmcmullen@mail.greenriver.edu>
2021-02-04 14:19:37 -0500

    Added composer and index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//This is my CONTROLLER
+/** Create a pet order form */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//define a default route(home page)
+$f3->route('GET /', function() {
+    echo "Home page";
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
